5658050c446a662863d23f85471dfa63337291e4
Phantom <kenichixie@gmail.com>
2017-03-16 23:16:42 +0700

    initial

---
+++ src/util/DbConn.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Phantom
+ */
+
+//AKA connection String
+public class DbConn {
+    public static final String JDBC_CLASS="com.mysql.jdbc.Driver";
+    public static final String JDBC_URL="jdbc:mysql://localhost:3306/payroll";
+    public static final String JDBC_USERNAME="root";
+    public static final String JDBC_PASSWORD="12345678";
+    
+}
